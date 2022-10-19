be5e247f190d61bbfc6918bccdfb4260f36a143e
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-19 19:44:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
 
 
 
